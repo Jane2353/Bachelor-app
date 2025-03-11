3346825e0c9f607882d94f8f8f38e7d65e4ee210
Jane2353 <Janevoetmann@gmail.com>
2025-03-11 21:41:59 +0100

    MitID login page

--- App.js
+++ App.js
@@ -1,20 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image } from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
-export default function App() {
-  return (
+const HelloWorldApp = () => {
+
+  return(
     <View style={styles.container}>
-      <Text>:-)</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.textTitle}>Log på med MitID </Text>
+        <Image style={styles.mitidIcon} source={require('./assets/mitid.png')} />
+      </View>
+
+      <View style={styles.line}>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>BRUGER-ID</Text>
+        <TextInput style={styles.textInput} />
+      </View>
+
+      <TouchableOpacity style={styles.buttonCont}>
+        <Text style={styles.buttonText}>FORTSÆT</Text>
+        <Image style={styles.buttonIcon} source={require('./assets/right-arrow.png')} />
+      </TouchableOpacity>
+
     </View>
-  );
-}
+    )
+  }
 
 const styles = StyleSheet.create({
+
+  /* Main container code */
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  /* Text container */
+  inputContainer: { 
+    width: '70%',
+    paddingBottom: 10,
+  },
+
+  /* Log på MitID code */
+  titleContainer: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    width: '70%',  
+    flexDirection: 'row',
+  },
+  textTitle: {
+    color: 'black',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  mitidIcon: {
+  width: 50, 
+  height: 40,
+  resizeMode: 'contain',
+  },
+
+  /* Line code */
+  line: {
+    width: '70%', 
+    height: 1,     
+    backgroundColor: '#c8c8c8',
+    marginBottom: 50
+  },
+
+  /* BRUGER-ID */
+  text: {
+    color: '#383838',
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+  },
+
+  /* Text input code */
+  textInput: {
+    borderWidth: 2,
+    borderColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+    fontSize: 15,
+  },
+
+  /* Button code */
+  buttonCont: {
+    backgroundColor: '#007AFF',
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 1,
+    height: 55,
+    width: "70%",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+  buttonText: {
+      color: 'white',
+      fontSize: 15,
+      fontWeight: 'light',
+  },
+  buttonIcon: {
+    tintColor: 'white',
+    width: 30,
+    height: 40,
+    resizeMode: 'contain',
+  }
 });
+
+export default HelloWorldApp;
