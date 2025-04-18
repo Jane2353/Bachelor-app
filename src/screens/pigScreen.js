98ba79d4be273a6bbb2e3d9c7c0a073b9f68ac5e
Jane2353 <Janevoetmann@gmail.com>
2025-04-18 15:29:29 +0200

    Changed name of images, so theyre easier to use. Added changes in the pigScreen, added the top three buttons 'Home' 'Challenge' and 'Shop'. Added a grey box to indicate where the moodmeter will be and added pig icon.

--- src/screens/pigScreen.js
+++ src/screens/pigScreen.js
@@ -4,10 +4,70 @@ import Popup from '../components/popup';
 
 const PigScreen = () => {
   return (
-    <View>
-      <Text>Piggy</Text>
+    <View style={styles.container}>
+      <Text style={styles.Title}>Piggy is happy to see you!</Text>
+    <View style={styles.containerButtons}>
+      <TouchableOpacity style={styles.blackButton}>Home</TouchableOpacity>
+      <TouchableOpacity style={styles.blackButtonUnclicked}>Challenge</TouchableOpacity>
+      <TouchableOpacity style={styles.blackButtonUnclicked}>Shop</TouchableOpacity>
     </View>
+    <View style={styles.containerMeter}></View>
+    <Image style={styles.pigIcon} source={require('../../assets/Pig/side_happy.png')}></Image>
+  </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+  },
+  Title: {
+    marginTop: '10%',
+    fontSize: 32,
+  },
+  containerButtons: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    marginTop: '10%',
+    width: '90%', 
+  },
+  blackButton: {
+    flex: 'row',
+    backgroundColor: 'black',
+    width: '32%',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20,
+    color: 'white',
+  },
+  blackButtonUnclicked: {
+    flex: 'row',
+    backgroundColor: 'white',
+    width: '32%',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20,
+    color: 'black',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  containerMeter: {
+    marginTop: '5%',
+    width: '90%',
+    height: '10%',
+    backgroundColor: 'grey',
+  },
+  pigIcon: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  }
+});
 
 export default PigScreen;
